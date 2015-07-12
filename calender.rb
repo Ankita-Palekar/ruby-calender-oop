8ea5ca4acaf233dd5a7a5fe47f41f90a71d2e140
Ankita-Palekar <453ankitapalekar@gmail.com>
2015-07-12 13:25:35 +0530

    display chanegd

--- calender.rb
+++ calender.rb
@@ -31,26 +31,40 @@ class Calender
 		flush_output
 	end
 
+	def print_prev_fake(num)
+		for i in 0..(num - 1)
+			print "     "
+		end
+	end
+
 	def display_dates
 		#calender fully dependent on the current_start_date
 		date = @current_start_date 
 		#logic to calc previous pseudo dates
 		prev_fake_month_days = @current_start_date.wday  
-
+		strt_ptr = date.wday
+		print_prev_fake(strt_ptr)
 
 		for week in 0..5
-			for day in 0..6
-				if day == date.wday
-					(date.month == @current_start_date.month)	? (print date.day) : (print "*", date.day)		
-					((date.day < 9) & (date.month == @current_start_date.month)) ? ( print "    ") :( print "   ")
-					date +=1	
-				else
-					 print "*", (@current_start_date - prev_fake_month_days).day , "  "
-					 prev_fake_month_days -= 1
-				end
+			for day in strt_ptr..6
+				(date.day < 9) ? (print date.day, "    ") : (print date.day,"   ")
+				date += 1 
+				break if date.month != @current_start_date.month
+				# if day == date.wday
+				# 	(date.month == @current_start_date.month)	? (print date.day) : (print "*", date.day)		
+				# 	((date.day < 9) & (date.month == @current_start_date.month)) ? ( print "    ") :( print "   ")
+				# 	date +=1	
+				# else
+				# 	 print "*", (@current_start_date - prev_fake_month_days).day , "  "
+				# 	 prev_fake_month_days -= 1
+				# end
 			end
+			strt_ptr = 0
 			print "\n"
 		end
+
+		# print_next_fake()
+
 		print "\n"
 	end
 
@@ -95,16 +109,11 @@ calender_calculator
 #
 #
 #
-#   July  2015
-#   S    M    T    W    T    F    S    
-#   *28  *29  *30  1    2    3    4    
-#   5    6    7    8    9   10   11   
-#   12   13   14   15   16   17   18   
-#   19   20   21   22   23   24   25   
-#   26   27   28   29   30   31   *1   
-#   *2   *3   *4   *5   *6   *7   *8   
-
-#
-#
-#
-#
+# 										July  2015
+# 				S    M    T    W    T    F    S    
+# 				*28  *29  *30  1    2    3    4    
+# 				5    6    7    8    9   10   11   
+# 				12   13   14   15   16   17   18   
+# 				19   20   21   22   23   24   25   
+# 				26   27   28   29   30   31   *1   
+# 				*2   *3   *4   *5   *6   *7   *8   
